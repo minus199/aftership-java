8cb14b3dbff4a9c90fc6120d40f9a97cba9b3347
MiNuS <minus199@gmail.com>
2015-11-30 22:08:29 +0200

    refactor

---
+++ src/main/java/com/aftership/sdk/Enums/StatusTag.java
@@ -0,0 +1,8 @@
+package com.minus.vendor.aftershipSdk.Classes.Enums;
+
+/**
+ * Created by User on 13/6/14.
+ */
+public enum StatusTag {
+    Pending,InTransit,OutForDelivery,AttemptFail,Delivered,Exception,Expired,InfoReceived
+}
